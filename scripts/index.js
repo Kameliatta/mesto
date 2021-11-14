7900ddb40f8469e1edbf4e09e17e20faf6bde4f3
Ksenia Prozorova <niakam41@yandex.ru>
2021-11-14 19:04:21 +1200

    sprint 8 without WebPack

--- scripts/index.js
+++ scripts/index.js
@@ -1,104 +1,105 @@
 import Card from "./Card.js";
 import {FormValidator} from "./FormValidator.js";
-import {initialCards} from "./initialCards.js";
-import {validationSettings} from "./validationSettings.js"
-
-const popupEdit = document.querySelector('#edit');
-const popupAdd = document.querySelector('#add');
-const popupImage = document.querySelector('#open-image');
-const closeEditButton = document.querySelector('#close-edit');
-const closeAddButton = document.querySelector('#close-add');
-const openEditButton = document.querySelector('.profile__edit-button');
-const openAddButton = document.querySelector('.profile__add-button');
-const editContainer = document.querySelector('#edit-container');
-const addContainer = document.querySelector('#add-container');
-const nameInput = document.querySelector('#name');
-const jobInput = document.querySelector('#description');
-const titleInput = document.querySelector('#title');
-const linkInput = document.querySelector('#link');
-const profileName = document.querySelector('.profile__name');
-const profileText = document.querySelector('.profile__text');
-const cardList = document.querySelector('.elements');
-const closeImageBtn = document.querySelector('#close-image');
-const bigImage = document.querySelector('#big-image');
-const textImage = document.querySelector('#image-text');
+import {initialCards} from "./utils/initialCards.js";
+import {validationSettings} from "./validationSettings.js";
+import Section from "./Section.js";
+import Popup from "./Popup.js";
+import PopupWithImage from "./PopupWithImage.js";
+import PopupWithForm from "./PopupWithForm.js";
+import UserInfo from "./UserInfo.js";
+import {
+  popupAdd,
+  popupImage, 
+  cardList,
+  openAddButton,
+  titleInput,
+  linkInput,
+  openEditButton,
+  popupEdit,
+  profileName,
+  profileText,
+  saveButton
+} from "./utils/constants.js";
 
 const validationFormEdit = new FormValidator(validationSettings, '#edit-container');
 validationFormEdit.enableValidation();
 const validationFormAdd = new FormValidator(validationSettings, '#add-container');
 validationFormAdd.enableValidation();
 
-initialCards.forEach( (element) => {
-  createCard(element);
+const popupWithImage = new PopupWithImage(popupImage);
+
+const addPopup = new PopupWithForm(popupAdd, createCard);
+
+const editPopup = new Popup(popupEdit);
+
+const userInfo = new UserInfo({
+  nameSelector: '.profile__name', 
+  aboutSelector: '.profile__text'
 });
 
-function createCard(element) {
-  const newCard = new Card(element, '.element-container_template', () => openPopup(popupImage), textImage, bigImage);
-  const cardElement = newCard.generateCard();
-  cardList.prepend(cardElement);
+function renderProfileInfo () {
+  userInfo.setUserInfo({
+    name: "Жак-Ив Кусто",
+    description: "Исследователь океана"
+  });
+  userInfo.updateUserInfo();
 }
 
-function openPopup(popup) { 
-  popup.classList.add('popup_opened');
-  document.addEventListener('keydown', closePopupByEsc);
+function changeUserInfo() {
+  userInfo.setUserInfo({
+    name: profileName.value,
+    description: profileText.value
+  })
+  userInfo.updateUserInfo();
+
+  editPopup.close();
 }
 
-function closePopup(popup) {
-  popup.classList.remove('popup_opened');
-  document.removeEventListener('keydown', closePopupByEsc);
-}
-
-function openEditPopup() { 
-  nameInput.value = profileName.textContent;
-  jobInput.value = profileText.textContent;
-  openPopup(popupEdit);
-}
-
-function handleProfileFormSubmit(evt) {
-    evt.preventDefault();
-    profileName.textContent = nameInput.value;
-    profileText.textContent = jobInput.value;
-
-    closePopup(popupEdit);
-};
-
-const renderCard = (evt) => {
-  evt.preventDefault();
-  const cardValue = {
-    name: titleInput.value,
-    link: linkInput.value,
-  };;
-  createCard(cardValue);
-  closePopup(popupAdd);
+function createCard ({name, link}) {
+  renderCard({
+    name: name, 
+    link: link
+  });
+  addPopup.close();
   titleInput.value = '';
   linkInput.value = '';
 }
 
-function closePopupByEsc(evt) {
-  const openPopupClass = document.querySelector('.popup_opened');
-  if (evt.key === 'Escape') {
-    closePopup(openPopupClass);
-  }
-} 
+const renderedCard = new Section({
+    items: initialCards,
+    renderer: (element) => renderCard(element)
+  }, 
+  cardList
+);
 
-function closeByOverlay(evt) {
-  if (evt.target.classList.contains('popup_opened')) {
-    closePopup(evt.target);
-  }
+function renderCard(element) {
+  const newCard = new Card(element, '.element-container_template', handleCardClick);
+  const cardElement = newCard.generateCard();
+  renderedCard.addItem(cardElement);
 }
-openEditButton.addEventListener('click', openEditPopup);
-openAddButton.addEventListener('click', () => {
+
+function handleCardClick(name, link) {
+  popupWithImage.open(name, link);
+}
+
+function openAddPopup() {
   validationFormAdd.toggleButtonState(false);
-  openPopup(popupAdd);
-});
-closeEditButton.addEventListener('click', () => closePopup(popupEdit));
-closeAddButton.addEventListener('click', () => {
-  closePopup(popupAdd)
-  validationFormAdd.clearForm();
-});
-closeImageBtn.addEventListener('click', () => closePopup(popupImage));
-editContainer.addEventListener('submit', handleProfileFormSubmit);
-addContainer.addEventListener('submit', renderCard);
-popupEdit.addEventListener('click', closeByOverlay);
-popupAdd.addEventListener('click', closeByOverlay);
-popupImage.addEventListener('click', closeByOverlay);
+  addPopup.open();
+}
+
+function openEditPopup() { 
+  profileName.value = userInfo.getUserInfo().name;
+  profileText.value = userInfo.getUserInfo().description;
+
+  editPopup.open();
+}
+
+openEditButton.addEventListener('click', openEditPopup)
+openAddButton.addEventListener('click', openAddPopup);
+saveButton.addEventListener('click', changeUserInfo);
+editPopup.setEventListeners();
+addPopup.setEventListeners();
+popupWithImage.setEventListeners();
+
+renderProfileInfo();
+renderedCard.renderItems();
